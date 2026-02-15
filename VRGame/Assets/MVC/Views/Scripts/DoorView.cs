7e4f9043c8f1c8a1eff10f4d70f1d72f6ea922e0
gvr812 <gvr812@usask.ca>
2026-02-15 15:33:16 -0600

    [Chore] Write docs for DoorView

--- VRGame/Assets/MVC/Views/Scripts/DoorView.cs
+++ VRGame/Assets/MVC/Views/Scripts/DoorView.cs
@@ -2,16 +2,43 @@
 
 using UnityEngine;
 using UnityEngine.Assertions;
+
+/// <summary>
+/// View portion of the reusable door module. Collisions are handled here
+/// </summary>
+/// <var
+/// <remarks>
+/// - doorController is always non-null upon calling Init()
+/// </remarks>
 public class DoorView : MonoBehaviour, IDoorView
 {
+    
+    /// Controller portion of door module. The view portion will
+    /// call methods of this.
     private IDoorController doorController;
 
+    /// <summary>
+    ///  Public accessor of the controller portion of the door module.
+    /// </summary>
     public IDoorController DoorController
     {
+        /// <summary>
+        /// Access the DoorView's DoorController instance variable
+        /// </summary>
         get
         {
             return doorController;
         }
+        /// <summary>
+        /// Set the value of the DoorView's DoorController instance variable
+        /// Note: This is for unit testing purposes - the instance variables of MonoBehaviour
+        /// scripts are usually set in a GUI window within the Unity editor 
+        /// </summary>
+        /// <remarks>
+        /// Preconditions:
+        /// - `value` must be non-null
+        /// Postconditions:
+        /// - DoorView's `doorController` instance variable set to input value.
         set
         {
             Assert.IsNotNull(value, "doorController cannot be null.");
@@ -20,19 +47,55 @@ public class DoorView : MonoBehaviour, IDoorView
     }
 
 
+    /// <summary>
+    /// Initializes the DoorView. Called by the game within the
+    /// MonoBehaviour function `Start()` (executes the frame a script is enabled)
+    /// - Separated from `Start()`, as this makes unit testing easier.
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - `doorController` instance var must be non-null
+    /// PostConditions:
+    /// - Checked to make sure DoorView will be able to function properly, has necessary values set for instance vars
+    /// </remarks>
     public void Init()
     {
         Assert.IsNotNull(doorController, "Field doorController cannot be null.");
     }
 
+    /// <summary>
+    /// Called when another object's collider enters this door's collider.
+    /// Handles player collision with door.
+    /// </summary>
+    /// <param name="other">Collider than has interacted with this door's collider</param>
+    /// <remarks>
+    /// Preconditions:
+    /// - `Collider other` must be non-null
+    /// - `doorController` instance var must be non-null
+    public void OnTriggerEnter(Collider other)
+    {
+        Assert.IsNotNull(other, "Collider other can not be null.");
+
+        // Ignore interaction with any collider that is not the player's.
+        if (!other.gameObject.CompareTag("MainCamera")) {
+            return;
+        }
+
+        doorController.OnPlayerEnter(other.GetComponentInParent<IPlayerModel>().gameObject);
+    }
+
+    /// <summary>
+    /// A `MonoBehaviour` function, called on the frame when a script is enabled, before
+    /// any `Update()` functions are called
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - `doorController` instance var must be non-null
+    /// PostConditions:
+    /// - Checked to maked sure DoorView will be able to function properly, has necessary values set for instance vars
+    /// </remarks>
     private void Start()
     {
         Init();
     }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (!other.gameObject.CompareTag("MainCamera")) return;
-    }
-
 }
