9d34b86b427f875b8a2f1dddd7a83e656dad914e
charlesgamboa <charlesgamboa15@gmail.com>
2026-02-17 17:17:06 -0600

    [Fix] Fix Branch Issues
    My local branch wasn't pushing to remote branch.

---
+++ VRGame/Assets/MVC/Controllers/Rooms/RoomControllerInterface.cs
@@ -0,0 +1,18 @@
+public interface IRoomController
+{
+    /// DATA SECTION
+
+    /// <summary>
+    /// Handles the logic required when the room becomes completed.
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - The room is not previously completed.
+    /// - All educational dialogue completed.
+    /// - Minigame is completed.
+    /// Postconditions:
+    /// - The room's completion state is updated.
+    /// - Any completion-related effects or callbacks have been executed.
+    /// </remarks>
+    void HandleCompletion();
+}
