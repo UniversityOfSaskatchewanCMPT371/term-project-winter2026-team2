84e50b04b40f5991efe3cb84c106c14750b2d00a
gvr812 <gvr812@usask.ca>
2026-02-17 08:52:37 -0600

    [Feat] Issue serializing dictionary, for simplicity using enum to index scenes insted. Refactoring sceneChanger

---
+++ VRGame/Assets/MVC/Models/Scripts/SceneEnums.cs
@@ -0,0 +1,16 @@
+
+
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Enum used to index scenes. Map directly to build index numbers
+/// </summary>
+public enum Scenes
+{
+    Hub = 0,
+}
