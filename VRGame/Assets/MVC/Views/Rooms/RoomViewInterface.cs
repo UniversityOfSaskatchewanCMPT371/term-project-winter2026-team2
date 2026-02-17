41e73e46ee6f1d8c46fc2a4f5ef57b11a7f9afc2
charlesgamboa <charlesgamboa15@gmail.com>
2026-02-17 17:39:43 -0600

    [Feat] Completed Rooms Interfaces
    Rooms interfaces have been completed.

--- VRGame/Assets/MVC/Views/Rooms/RoomViewInterface.cs
+++ VRGame/Assets/MVC/Views/Rooms/RoomViewInterface.cs
@@ -1,4 +1,39 @@
 public interface IRoomView
 {
-    event System.Action OnRoomCompletion;
+    /// DATA SECTION
+
+    /// <summary>
+    /// Called when all educational dialogues and minigame is completed.
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - The room is in a valid state of compeletion.
+    /// Postconditions:
+    /// - All subscribed listeners to this event is executed.
+    /// </remarks>
+    event System.Action OnRoomCompleted;
+
+    /// METHODS SECTIONS
+
+    /// <summary>
+    /// Called when all educational dialogues are complete.
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - All educational dialogues are completed.
+    /// Postconditions:
+    /// - The room's educational dialogue completion state is updated.
+    /// </remarks>
+    void EducationalDialoguesCompleted();
+
+    /// <summary>
+    /// Called when the minigame is completed.
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - Minigame is completed.
+    /// Postconditions:
+    /// - The room's minigame compeltion state is updated.
+    /// </remarks>
+    void MinigameCompleted();
 }
