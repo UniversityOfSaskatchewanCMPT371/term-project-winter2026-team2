0ee6542589fdd11a3c03be7cddd95ced2c71dfc7
reyna <reynanncastrojr@gmail.com>
2026-02-21 17:48:26 -0600

    [Chore[ started on tooltiptip model tests editmode

--- Assets/Tests/MVC/Model/EditMode/ToolTip/ToolTipModelTests.cs
+++ Assets/Tests/MVC/Model/EditMode/ToolTip/ToolTipModelTests.cs
@@ -6,20 +6,35 @@ using UnityEngine.TestTools;
 
 public class ToolTipModelTests
 {
+
+    private ToolTipModel toolTipModel;
+
+    [SetUp]
+    public void SetUp()
+    {
+        toolTipModel = (ToolTipModel)ScriptableObject.CreateInstance(typeof(ToolTipModel));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        UnityEngine.Object.DestroyImmediate(toolTipModel);
+    }
+
     // A Test behaves as an ordinary method
     [Test]
-    public void ToolTipModelTestsSimplePasses()
+    public void Title_ShouldBeNull_WhenInitialized()
     {
-        // Use the Assert class to test conditions
+        Assert.IsNull(toolTipModel.Title);
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
-    [UnityTest]
+/*     [UnityTest]
     public IEnumerator ToolTipModelTestsWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
         yield return null;
-    }
+    } */
 }
