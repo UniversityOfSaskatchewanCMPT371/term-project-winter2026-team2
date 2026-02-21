42175d00c610a0b7a88a435f912c8cf865bfce60
reyna <reynanncastrojr@gmail.com>
2026-02-21 17:37:56 -0600

    [Feature] Add Tests for tooltipcontroller editmode

--- Assets/Tests/MVC/Controller/EditMode/ToolTip/ToolTipControllerTests.cs
+++ Assets/Tests/MVC/Controller/EditMode/ToolTip/ToolTipControllerTests.cs
@@ -3,23 +3,54 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using NSubstitute;
+using System;
 
 public class ToolTipControllerTests
 {
+
+    private GameObject interactiveElement;
+    private IToolTipTrigger trigger;
+    private ToolTipController toolTipController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        interactiveElement = new GameObject("InteractiveElement");
+        trigger = Substitute.For<IToolTipTrigger>();
+        toolTipController = new ToolTipController(interactiveElement, trigger);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        toolTipController.Dispose();
+        UnityEngine.Object.DestroyImmediate(interactiveElement);
+    }
+
     // A Test behaves as an ordinary method
     [Test]
-    public void ToolTipControllerTestsSimplePasses()
+    public void Constructor_DisablesInteractiveElement()
     {
-        // Use the Assert class to test conditions
+        Assert.IsFalse(interactiveElement.activeSelf);
     }
+
+    [Test]
+    public void OnHoverEnter_EnablesInteractiveElement()
+    {
+        //raise hoverentered event on the mock
+        trigger.HoverEntered += Raise.Event<Action>();
+        Assert.IsTrue(interactiveElement.activeSelf);
+    }
+    
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
-    [UnityTest]
+/*     [UnityTest]
     public IEnumerator ToolTipControllerTestsWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
         yield return null;
-    }
+    } */
 }
