f494dbc44d28994ae98ebf274f4ae0b3e1d90970
antoinetterubia <mar092@usask.ca>
2026-02-20 18:08:45 -0600

    [Chore] Created a player model script
    Will be used for player spawning

---
+++ VRGame/Assets/MVC/Models/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A simple PlayerModel class to represent the player in the MVC architecture
+/// </summary>
+public class PlayerModel : MonoBehaviour
+{
+    // Basic player attributes
+    public String playerName;
+    public int id;
+
+    ///<summary>
+    /// Initializes the player model with a name and an ID
+    /// </summary>
+    /// <param name="name">The name of the player</param>
+    /// <param name="id">The unique identifier for the player</param>
+    public void Initialize(string name, int id)
+    {
+        this.playerName = name;
+        this.id = id;
+    }
+
+    /// <summary>
+    /// Getter method to retrieve the player's name
+    /// </summary>
+    /// pre-condition: 
+    ///     -   playerName has been initialized in the constructor
+    /// post-condition: 
+    ///     -   returns the name of the player
+    public String getPlayerName()
+    {
+        return playerName;
+    }
+
+    /// <summary>
+    /// Getter method to retrieve the player's ID
+    /// </summary>
+    /// pre-condition:
+    ///     -   id has been initialized in the constructor
+    /// post-condition:
+    ///    -   returns the unique identifier of the player
+    public int getId()
+    {
+        return id;
+    }
+}
