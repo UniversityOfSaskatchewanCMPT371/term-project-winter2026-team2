ab885764a2c86224d303b49f4a63791d988dab5a
reyna <reynanncastrojr@gmail.com>
2026-02-21 08:40:50 -0600

    [Fix] Update ToolTipTrigger for Interface

--- Assets/MVC/Controller/Scripts/ToolTipTrigger.cs
+++ Assets/MVC/Controller/Scripts/ToolTipTrigger.cs
@@ -9,6 +9,11 @@ public class ToolTipTrigger : MonoBehaviour
     public GameObject interactiveElement;
     public XRBaseInteractable interactable;
 
+    public IXRInteractable Interactable
+    {
+        get; set;
+    }
+
 
     //<summary>
     // PreCondition: The interactiveElement GameObject must be assigned in the Unity Editor.
@@ -18,13 +23,19 @@ public class ToolTipTrigger : MonoBehaviour
     /// <param name="state"></param>
     // Shows or hides the interactive element based on the provided state.
     // </summary>
-    public void show(bool state)
+/*     public void show(bool state)
     {
         interactiveElement.SetActive(state);
-    }
+    } */
     void Awake()
     {
-        interactable = GetComponent<XRBaseInteractable>();
+
+        if (Interactable == null)
+        {
+            //use the adapter
+            interactable = GetComponent<XRBaseInteractable>();
+            Interactable = new XRInteractableAdapter(interactable);
+        }
         
     }
 
@@ -32,8 +43,10 @@ public class ToolTipTrigger : MonoBehaviour
     void Start()
     {
         interactiveElement.SetActive(false);
-        interactable.hoverEntered.AddListener(OnHoverEnter);
-        interactable.hoverExited.AddListener(OnHoverExit);
+        /* interactable.hoverEntered.AddListener(OnHoverEnter);
+        interactable.hoverExited.AddListener(OnHoverExit); */
+        Interactable.HoverEntered += OnHoverEnter;
+        Interactable.HoverExited += OnHoverExit;
     }
     // <summary>
     // PreCondition: The hover event must be triggered by an XR controller or pointer.
@@ -42,7 +55,8 @@ public class ToolTipTrigger : MonoBehaviour
     /// 
     private void OnHoverEnter(HoverEnterEventArgs args)
     {
-        show(true);
+        // show(true);
+        interactiveElement.SetActive(true);
     }
     // <summary>
     // PreCondition: The hover event must be triggered by an XR controller or pointer.
@@ -51,12 +65,24 @@ public class ToolTipTrigger : MonoBehaviour
     /// </summary>
     private void OnHoverExit(HoverExitEventArgs args)
     {
-        show(false);
+        // show(false);
+        interactiveElement.SetActive(false);
     }
 
-    // Update is called once per frame
+    void OnDestroy()
+    {
+        if (Interactable != null)
+        {
+            Interactable.HoverEntered -= OnHoverEnter;
+            Interactable.HoverExited -= OnHoverExit;
+        }
+       
+    }
+
+        //commented this out i was getting Leak Detected idk it disappeared
+/*      // Update is called once per frame
     void Update()
     {
         
-    }
+    }  */
 }
