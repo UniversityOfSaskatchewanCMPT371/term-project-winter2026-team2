e31016e88376098ccdaf225ad4e5704d836dc99c
reyna <reynanncastrojr@gmail.com>
2026-02-21 17:55:29 -0600

    [Chore] Start on editmode test for tooltipview

--- Assets/Tests/MVC/View/EditMode/ToolTip/ToolTipViewTests.cs
+++ Assets/Tests/MVC/View/EditMode/ToolTip/ToolTipViewTests.cs
@@ -3,23 +3,60 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using TMPro;
+using NSubstitute;
 
 public class ToolTipViewTests
 {
+    private GameObject toolTipGameObject;
+    private ToolTipView toolTipView;
+    private IToolTipModel toolTipModel;
+
+    [SetUp]
+    public void SetUp()
+    {
+        //root gameobject for the view
+        toolTipGameObject = new GameObject("ToolTip");
+        toolTipView = toolTipGameObject.AddComponent<ToolTipView>();
+        
+        //child gameobjects for title and description
+        var titleGo = new GameObject("Title");
+        titleGo.transform.SetParent(toolTipGameObject.transform);
+        toolTipView.title = titleGo.AddComponent<TextMeshProUGUI>();
+
+        //description child gameobject
+        var descriptionGo = new GameObject("Description");
+        descriptionGo.transform.SetParent(toolTipGameObject.transform);
+        toolTipView.description = descriptionGo.AddComponent<TextMeshProUGUI>();
+
+        //mock of model interface
+        toolTipModel = Substitute.For<IToolTipModel>();
+        toolTipModel.Title.Returns("Test Title");
+        toolTipModel.Description.Returns("Test Description");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        UnityEngine.Object.DestroyImmediate(toolTipGameObject);
+    }
+
     // A Test behaves as an ordinary method
     [Test]
-    public void ToolTipViewTestsSimplePasses()
+    public void UpdateContent_SetsTextFromModel()
     {
-        // Use the Assert class to test conditions
+        toolTipView.UpdateContent(toolTipModel);
+        Assert.AreEqual("Test Title", toolTipView.title.text);
+        Assert.AreEqual("Test Description", toolTipView.description.text);
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
-    [UnityTest]
+/*     [UnityTest]
     public IEnumerator ToolTipViewTestsWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
         yield return null;
-    }
+    } */
 }
