8bfbeb3f9579f55e37581e7668b2ba5918d087e8
leif-motif <140660659+leif-motif@users.noreply.github.com>
2026-02-21 21:51:16 -0600

    [FEAT] better balltest, got inputactions working

--- VRGame/Assets/MVC/Controllers/XRI/BallTest.cs
+++ VRGame/Assets/MVC/Controllers/XRI/BallTest.cs
@@ -7,17 +7,37 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class BallTest : MonoBehaviour {
     private XRGrabInteractable grabInteractable;
+    private XRIInputActions inputActions;
 
-    // Start is called before the first frame update
+    private void Awake(){
+        inputActions = new XRIInputActions();
+    }
+
     void Start(){
         grabInteractable = GetComponent<XRGrabInteractable>();
         if(grabInteractable != null){
             Debug.Log("We're good to go!");
         }
-        
+    }
+
+    private void OnEnable(){
+        inputActions.Enable();
+        inputActions.XRIRightInteraction.UIPress.performed += OnTrigger;
+    }
+
+
+    private void OnDisable(){
+        inputActions.XRIRightInteraction.UIPress.performed -= OnTrigger;
+        inputActions.Disable();
     }
 
     private void OnTrigger(InputAction.CallbackContext context){
-        Debug.Log("Bam!");
+        if(IsHeld()){
+            Debug.Log("Bam!");
+        }
+    }
+
+    public bool IsHeld(){
+        return grabInteractable.interactorsSelecting.Count > 0;
     }
 }
