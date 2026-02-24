8c420e6f867afafc0e735b9e66d33b600c469d40
reyna <reynanncastrojr@gmail.com>
2026-02-24 13:02:30 -0600

    [Chore] fixed comments

--- VRGame/Assets/MVC/Controllers/Scripts/ToolTipTrigger.cs
+++ VRGame/Assets/MVC/Controllers/Scripts/ToolTipTrigger.cs
@@ -31,17 +31,13 @@ public class ToolTipTrigger : MonoBehaviour, IToolTipTrigger
     void Awake()
     {
          interactable = GetComponent<XRBaseInteractable>();
-           
+ 
         
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        /*interactiveElement.SetActive(false);
-          interactable.hoverEntered.AddListener(OnHoverEnter);
-        interactable.hoverExited.AddListener(OnHoverExit); */
-
         //forward XR events to our own events
         interactable.hoverEntered.AddListener(_=> HoverEntered?.Invoke());
         interactable.hoverExited.AddListener(_=> HoverExited?.Invoke());
@@ -50,37 +46,11 @@ public class ToolTipTrigger : MonoBehaviour, IToolTipTrigger
         toolTipController = new ToolTipController(interactiveElement, this);
         
     }
-    // <summary>
-    // PreCondition: The hover event must be triggered by an XR controller or pointer.
-    /// PostCondition: The interactive element will be shown when the hover event is entered and hidden when the hover event is exited.
-    /// <param name="args"></param>
-    /// 
-/*     private void OnHoverEnter(HoverEnterEventArgs args)
-    {
-        // show(true);
-        interactiveElement.SetActive(true);
-    } */
-    // <summary>
-    // PreCondition: The hover event must be triggered by an XR controller or pointer.
-    /// PostCondition: The interactive element will be hidden when the hover event is exited.
-    /// <param name="args"></param>
-    /// </summary>
- /*    private void OnHoverExit(HoverExitEventArgs args)
-    {
-        // show(false);
-        interactiveElement.SetActive(false);
-    }
- */
+
     void OnDestroy()
     {
         toolTipController?.Dispose();
        
     }
 
-        //commented this out i was getting Leak Detected idk it disappeared
-     // Update is called once per frame
-/*     void Update()
-    {
-        
-    }   */
 }
