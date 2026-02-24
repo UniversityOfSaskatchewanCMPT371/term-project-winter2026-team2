423ea0c664c78e40a95f990f47aa04bf5e155947
antoinetterubia <mar092@usask.ca>
2026-02-24 00:32:04 -0600

    [Fix] Removes one assertion line in SoH model
    No point keeping it if we're using Debug.LogWarning for null linkedObjects.
    Also because copilot suggested it on pr review

--- VRGame/Assets/MVC/Models/Scripts/ScaleOnHover/ScaleOnHoverModel.cs
+++ VRGame/Assets/MVC/Models/Scripts/ScaleOnHover/ScaleOnHoverModel.cs
@@ -418,7 +418,6 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
         {
             Debug.LogWarning("Cannot initialize scales. linked objects array is empty on Awake");
         }
-        Assert.IsNotNull(linkedObjects, "Linked objects array cannot be null on Awake");
         Assert.IsTrue(linkedObjects.Length > 0, "Linked objects array must have at least one element on Awake");
 
         /// All checks passed
