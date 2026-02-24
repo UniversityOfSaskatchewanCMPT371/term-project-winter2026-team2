67ca2cb7900c16133f8ba3fb68f7b652a7f7e40a
reyna <reynanncastrojr@gmail.com>
2026-02-24 17:27:46 -0600

    [Chore] cleaned comments

--- VRGame/Assets/Tests/MVC/Controllers/EditMode/ToolTip/ToolTipControllerTests.cs
+++ VRGame/Assets/Tests/MVC/Controllers/EditMode/ToolTip/ToolTipControllerTests.cs
@@ -88,14 +88,4 @@ public class ToolTipControllerTests
         Assert.IsFalse(interactiveElement.activeSelf);
     }
     
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-/*     [UnityTest]
-    public IEnumerator ToolTipControllerTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    } */
 }
