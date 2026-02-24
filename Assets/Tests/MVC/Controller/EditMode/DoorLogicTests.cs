833ff88a60c3ac5ad500edd2d83c313658ad9869
Joseph Obeng <53978801+whitesparro@users.noreply.github.com>
2026-02-24 02:01:24 -0600

    Test file to check the behavior of Resuable doors and scene transition created.

---
+++ Assets/Tests/MVC/Controller/EditMode/DoorLogicTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using System.Reflection;
+
+/// <summary>
+/// Test class for DoorLogic component in edit mode.
+/// This class contains unit tests to verify the behavior of the DoorLogic controller.
+/// </summary>
+public class DoorLogicTests
+{
+    /// <summary>
+    /// GameObject representing the door in the test scene.
+    /// </summary>
+    private GameObject doorObject;
+
+    /// <summary>
+    /// Instance of DoorLogic component being tested.
+    /// </summary>
+    private DoorLogic doorLogic;
+
+    /// <summary>
+    /// Data model for the door configuration.
+    /// </summary>
+    private DoorData doorData;
