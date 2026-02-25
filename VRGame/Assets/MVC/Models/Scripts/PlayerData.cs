ee42fd9b08a9bcd818498e23def07dc6a8f6ecfe
crh082 <crh082@usask.ca>
2026-02-25 12:28:48 -0600

    [Chore] Added missing pre & post conditions for docstring for PlayerData.cs
    As per requested changes for hub2 pull request

--- VRGame/Assets/MVC/Models/Scripts/PlayerData.cs
+++ VRGame/Assets/MVC/Models/Scripts/PlayerData.cs
@@ -8,6 +8,14 @@ public class PlayerData : MonoBehaviour
     /// <summary>
     /// Ensures this component follows a singleton pattern and persists across scene loads.
     /// </summary>
+    /// <remarks>
+    /// <pre-condition>
+    ///     -   This script is attached to a GameObject in the scene
+    /// </pre-condition>
+    /// <post-condition>
+    ///     -   Only one instance of PlayerData exists and it persists across scene loads
+    /// </post-condition>
+    /// </remarks>
     private void Awake()
     {
         if (instance != null && instance != this)
