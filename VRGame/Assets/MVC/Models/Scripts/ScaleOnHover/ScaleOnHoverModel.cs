ecd36de8c3cd2949f1e7e444e7ac1a0968d1f0c7
crh082 <crh082@usask.ca>
2026-02-24 22:02:45 -0600

    [Chore] Changed docstring format for ScaleOnHoverModel.cs
    As per requested changes for hub2 pull request

--- VRGame/Assets/MVC/Models/Scripts/ScaleOnHover/ScaleOnHoverModel.cs
+++ VRGame/Assets/MVC/Models/Scripts/ScaleOnHover/ScaleOnHoverModel.cs
@@ -11,31 +11,16 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     /// </summary>
     [SerializeField] private Transform[] linkedObjects;
 
-    /// <summary>
-    /// Public accessor method for linked objects
-    /// </summary>
+    /// <inheritdoc/>
     public Transform[] LinkedObjects
     {
-        /// <summary>
-        /// Getter method for linked objects
-        /// </summary>
-        /// <post-condition>
-        ///     - Returns the array of linked transform objects
-        /// </post-condition>
+        /// <inheritdoc/>
         get
         {
             return linkedObjects;
         }
 
-        /// <summary>
-        /// Setter method for linked objects
-        /// </summary>
-        /// <pre-condition>
-        ///     -   value != null && value.length > 0
-        /// </pre-condition>
-        /// <post-condition>
-        ///     -   linkedObjects is updated to the new array of transform objects
-        /// </post-condition>
+        /// <inheritdoc/>
         set
         {
             if (value == null || value.Length == 0)
@@ -55,31 +40,16 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     /// </summary>
     [SerializeField] private float hoverScaleMultiplier = 1.25f;
 
-    /// <summary>
-    /// Public accessor method for hover scale multiplier
-    /// </summary>
+    /// <inheritdoc/>
     public float HoverScaleMultiplier
     {
-        /// <summary>
-        /// Getter method for hover scale multiplier
-        /// </summary>
-        /// <post-condition>
-        ///     -   Returns the hover scale multiplier value
-        /// </post-condition>
+        /// <inheritdoc/>
         get
         {
             return hoverScaleMultiplier;
         }
 
-        /// <summary>
-        /// Setter method for hover scale multiplier
-        /// </summary>
-        /// <pre-condition>
-        ///     -   value > 0
-        /// </pre-condition>
-        /// <post-condition>
-        ///     -   Hover scale multiplier is updated to the new value
-        /// </post-condition>
+        /// <inheritdoc/>
         set
         {
             if (value < 0)
@@ -99,31 +69,16 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     /// </summary>
     [SerializeField] private float scaleSpeed = 10f;
 
-    /// <summary>
-    /// Public accessor method for scale speed
-    /// </summary>
+    /// <inheritdoc/>
     public float ScaleSpeed
     {
-        /// <summary>
-        /// Getter method for scale speed
-        /// </summary>
-        /// <post-condition>
-        ///     -   Returns the scale speed value
-        /// </post-condition>
+        /// <inheritdoc/>
         get
         {
             return scaleSpeed;
         }
 
-        /// <summary>
-        /// Setter method for scale speed
-        /// </summary>
-        /// <pre-condition>
-        ///     -   value > 0
-        /// </pre-condition>
-        /// <post-condition>
-        ///     -   Scale speed is updated to the new value
-        /// </post-condition>
+        /// <inheritdoc/>
         set
         {
             if (value < 0)            
@@ -143,17 +98,10 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     /// </summary>
     private Vector3[] normalScales;
 
-    /// <summary>
-    /// Public accessor method for normal scales
-    /// </summary>
+    /// <inheritdoc/>
     public Vector3[] NormalScales
     {
-        /// <summary>
-        /// Getter method for normal scales
-        /// </summary>
-        /// <post-condition>
-        ///     -   Returns the array of normal scales for linked objects
-        /// </post-condition>
+        /// <inheritdoc/>
         get
         {            
             return normalScales;
@@ -170,31 +118,16 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     /// </summary>
     private Vector3[] targetScales;
 
-    /// <summary>
-    /// Public accessor method for target scales
-    /// </summary>
+    /// <inheritdoc/>
     public Vector3[] TargetScales
     {
-        /// <summary>
-        /// Getter method for target scales
-        /// </summary>
-        /// <post-condition>
-        ///     -   Returns the array of target scales for linked objects
-        /// </post-condition>
+        /// <inheritdoc/>
         get
         {            
             return targetScales;
         }
 
-        /// <summary>
-        /// Setter method for target scales
-        /// </summary>
-        /// <pre-condition>
-        ///     -   value != null
-        /// </pre-condition>
-        /// <post-condition>
-        ///     -   Target scales are updated to the new values
-        /// </post-condition>
+        /// <inheritdoc/>
         set
         {
             if (value == null)
@@ -214,17 +147,10 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     /// </summary>
     private bool isHovering = false;
 
-    /// <summary>
-    /// Public accessor method for isHovering
-    /// </summary>
+    /// <inheritdoc/>
     public bool IsHovering
     {
-        /// <summary>
-        /// Getter method for isHovering
-        /// </summary>
-        /// <post-condition>
-        ///     -   Returns true if the object is currently being hovered, false otherwise
-        /// </post-condition>
+        /// <inheritdoc/>
         get
         {
             return isHovering;
@@ -234,20 +160,7 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
     }
 
 
-    /// <summary>
-    /// Initializes the model with parameters
-    /// </summary>
-    /// <param name="linkedObjects">Objects linked to the script</param>
-    /// <param name="hoverScaleMultiplier">How big the linkedObject will grow</param>
-    /// <param name="scaleSpeed">Speed of scale transitions</param>
-    /// <pre-condition>
-    ///     -   linkedObjects != null && linkedObjects.length > 0
-    ///     -   hoverScaleMultiplier > 0
-    ///     -   scaleSpeed > 0
-    /// </pre-condition>
-    /// <post-condition>
-    ///     -   Model is initialized with the parameters
-    /// </post-condition>
+    /// <inheritdoc/>
     public void Initialize(Transform[] linkedObjects, float hoverScaleMultiplier, float scaleSpeed)
     {
         // Debug logs for LinkedObjects parameter
@@ -288,15 +201,7 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
 
 
 
-    /// <summary>
-    /// Initializes the normal and target scales for objects linked
-    /// </summary>
-    /// <pre-condition>
-    ///      -   linkedObject != null && linkedObject > 0
-    /// </pre-condition>
-    /// <post-condition>
-    ///     -   The normal scale and target scale for a linked object is initialized
-    /// </post-condition>
+    /// <inheritdoc/>
     public void InitializeScales()
     {
         // Debug log for linkedObjects array
@@ -333,15 +238,7 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
 
 
 
-    /// <summary>
-    /// Called when hover enters - sets target scales to bigger values
-    /// </summary>
-    /// <pre-condition> 
-    ///     -   linkedObjects must exist and not be null
-    /// </pre-condition>
-    /// <post-condition> 
-    ///     -   Target scales are set to be bigger (1.25x)
-    /// </post-condition>
+    /// <inheritdoc/>
     public void OnHoverEnter()
     {
         isHovering = true;
@@ -365,15 +262,7 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
 
 
 
-    /// <summary>
-    /// Called when hover exits - sets target scales back to normal
-    /// </summary>
-    /// <pre-condition> 
-    ///     -   linkedObjects must exist and not be null
-    /// </pre-condition>
-    /// <post-condition> 
-    ///     -   Target scales goes back to normal scale
-    /// </post-condition>
+    /// <inheritdoc/>
     public void OnHoverExit()
     {
         isHovering = false;
@@ -397,15 +286,7 @@ public class ScaleOnHoverModel : MonoBehaviour, IScaleOnHoverModel
 
 
 
-    /// <summary>
-    /// Initialize scales on Awake (to the linkedObjects that are assigned in inspector)
-    /// </summary>
-    /// <pre-condition>
-    ///     -   0 < linkedObjects && linkedObjects != null
-    /// </pre-condition>
-    /// <post-condition>
-    ///     - Initializes the linkedObjects on Awake()
-    /// </post-condition>
+    /// <inheritdoc/>
     public void Awake()
     {
 
