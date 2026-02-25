12f2b76467378f588d014b64a7b8f064e6e5d32b
crh082 <crh082@usask.ca>
2026-02-25 13:28:18 -0600

    [Chore] Added Debug.LogWarning & Debug.LogError's to PlayerController.cs
    As per requested changes for hub2 pull request

--- VRGame/Assets/MVC/Controllers/Scripts/Player/PlayerController.cs
+++ VRGame/Assets/MVC/Controllers/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -12,11 +13,15 @@ public class PlayerController : MonoBehaviour, IPlayerController
         // Try to get components if not assigned
         if (model == null)
         {
+            Debug.LogWarning("PlayerModel reference was not set in inspector, trying to get component...");
             model = GetComponent<PlayerModel>();
+            Debug.LogErrorIf(model == null, "PlayerModel component not found on GameObject");
         }
         if (view == null)
         {
+            Debug.LogWarning("PlayerView reference was not set in inspector, trying to get component...");
             view = GetComponent<PlayerView>();
+            Debug.LogErrorIf(view == null, "PlayerView component not found on GameObject");
         }
 
         // Validate references
