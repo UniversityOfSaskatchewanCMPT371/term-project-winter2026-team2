0e0662f354fb5e36e23e6e6bb37c2ce3df642a14
crh082 <crh082@usask.ca>
2026-03-05 14:51:08 -0600

    [Chore] Added basic methods from IGridController to GridController
    Added all methods from interface, but no actual code is added yet

--- VRGame/Assets/Scripts/Logicpath/GridController.cs
+++ VRGame/Assets/Scripts/Logicpath/GridController.cs
@@ -2,3 +2,32 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
+
+public class GridController : MonoBehaviour, IGridController
+{
+    
+    /// <inheritdoc/>
+    public void Awake()
+    {
+
+    }
+
+    /// <inheritdoc/>
+    public void OnTriggeredPressed(Vector3 worldPosition)
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void ResetGrid()
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public bool CheckCompleted()
+    {
+        
+    }
+
+}
