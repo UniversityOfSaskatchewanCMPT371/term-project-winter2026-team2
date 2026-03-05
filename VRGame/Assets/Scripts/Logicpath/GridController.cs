9b11bd72c2c956eaeacfbd1e123d357a17e3795d
crh082 <crh082@usask.ca>
2026-03-05 13:28:58 -0600

    [Feat] Created barebones grid Model, View, and Controller classes
    Just the outlines, working on implementation now

---
+++ VRGame/Assets/Scripts/Logicpath/GridController.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
---
+++ VRGame/Assets/Scripts/Logicpath/GridView.cs
@@ -0,0 +1,3 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
