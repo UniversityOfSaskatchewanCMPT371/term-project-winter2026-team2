9f54ac1483d1986e46ba9a4ca84838f683d819d6
crh082 <crh082@usask.ca>
2026-03-05 14:45:15 -0600

    [Chore] Added basic methods from IGridView to GridView
    Added all methods from interface, but no actual code is added yet

--- VRGame/Assets/Scripts/Logicpath/GridView.cs
+++ VRGame/Assets/Scripts/Logicpath/GridView.cs
@@ -1,3 +1,50 @@
 using UnityEngine;
 using UnityEngine.Assertions;
 using System.Collections.Generic;
+
+public class GridView : MonoBehaviour, IGridView
+{
+
+    /// <inheritdoc/>
+    public void Initialize(int width, int height, float cellSize)
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void RenderPipe(int x, int y, Color color, Vector3 worldPosition)
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void RenderEndpoints(List<Endpoint> endpoints)
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void ClearAllPipes()
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void ShowCompletionEffect()
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void HighlightCell(Vector3 position)
+    {
+        
+    }
+
+    /// <inheritdoc/>
+    public void ClearHighlight()
+    {
+        
+    }
+
+}
