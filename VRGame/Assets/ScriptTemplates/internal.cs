e4b7710183de6cdadd2eea35a25e1ae8bd0f5fe2
ANISH4623 <anishpbhagat@gmail.com>
2026-03-09 18:13:37 -0600

    [Chore] Made access to ModelMock and ViewMock

--- VRGame/Assets/ScriptTemplates/internal.cs
+++ VRGame/Assets/ScriptTemplates/internal.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 // [assembly: InternalsVisibleTo("Name_Here")]
 [assembly: InternalsVisibleTo("ScriptTemplatesEditTests")]
+[assembly:InternalsVisibleTo("ObjectMatchGameEditModeTest")]
