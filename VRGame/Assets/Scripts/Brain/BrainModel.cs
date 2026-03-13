436d2bc9866c4e493a32708354ba7081a66cc174
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-13 15:11:04 -0600

    [Chore] Added Animator as data in BrainModel
    Will be used to access animation attached to 3DBrain in hub

--- VRGame/Assets/Scripts/Brain/BrainModel.cs
+++ VRGame/Assets/Scripts/Brain/BrainModel.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
-
 /// <summary>
 /// Model component of BrainModel.
 /// </summary>
 public class BrainModel : Model, IBrainModel
 {
+    private Animator animator;
 
     /// <inheritdoc/>
     public override void Init()
     {
-
+        this.animator = GetComponent<Animator>();
+        if (animator == null) {
+            Debug.LogError("Animator did not initialize properly in BrainModel Init()");
+        }
+        Assert.IsNotNull(animator, "Animator is null on Init()");
     }
 }
