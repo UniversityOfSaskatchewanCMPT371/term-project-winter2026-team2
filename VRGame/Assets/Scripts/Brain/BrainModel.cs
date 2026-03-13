d9049add7a7d616afcad5cbeef7dda1a3ac486d8
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-13 14:54:12 -0600

    [Chore] Added MVC scripts for a new Brain component
    need to access animation attached to Brain3D to implement pause on hover enter

---
+++ VRGame/Assets/Scripts/Brain/BrainModel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// Model component of BrainModel.
+/// </summary>
+public class BrainModel : Model, IBrainModel
+{
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+
+    }
+}
