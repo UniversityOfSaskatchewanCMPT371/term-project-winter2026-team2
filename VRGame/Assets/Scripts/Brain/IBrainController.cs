464ec3d43e0e7e0e553e5b9dddc3e53ab4dd509b
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-13 14:56:36 -0600

    [Chore] Added MVC interfaces using templates
    interfaces needed for Brain MVC component

---
+++ VRGame/Assets/Scripts/Brain/IBrainController.cs
@@ -0,0 +1,14 @@
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+// If you are making Model layer, inherit from IModel.
+// Same goes for other layers. (IController/IView)
+
+/// <summary>
+/// TODO: Change the docstring to match your implementation.
+/// </summary>
+public interface IBrainController : IModel
+{
+    /// <summary>
+    /// TODO: Change the docstring to match your implementation.
+    /// </summary>
+    public void Init();
+}
