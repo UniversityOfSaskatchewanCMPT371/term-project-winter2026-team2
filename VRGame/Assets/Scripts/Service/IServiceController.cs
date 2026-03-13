ff250303eec1acdcde10246cdb42b706733c33f5
charlesgamboa <charlesgamboa15@gmail.com>
2026-03-13 10:58:16 -0600

    [Chore] Documentation
    Move docstrings from both start() and awake() to the interface and used inheritdoc instead

--- VRGame/Assets/Scripts/Service/IServiceController.cs
+++ VRGame/Assets/Scripts/Service/IServiceController.cs
@@ -14,4 +14,29 @@ public interface IServiceController : IController
     /// - Logs warning if this instance is a duplicate. Otherwise, logs a successful initialization.
     /// </remarks>
     new void Init();
+
+    /// <summary>
+    /// Called once after the scene loads. 
+    /// Initializes this component by calling Init().
+    /// </summary>
+    /// <remarks>
+    /// Preconditions:
+    /// - Init() is implemented.
+    /// Postconditions:
+    /// - Init() is invoked.
+    /// </remarks>
+    void Awake();
+
+    /// <summary>
+    /// Called once after all Awake() calls.
+    /// This method does nothing, but overrides the default Start() defined in
+    /// Controller base class.
+    /// </summary
+    /// <remarks>
+    /// Preconditions:
+    /// - None
+    /// Postconditions:
+    /// - None
+    /// </remarks>
+    void Start();
 }
--- VRGame/Assets/Scripts/Service/ServiceController.cs
+++ VRGame/Assets/Scripts/Service/ServiceController.cs
@@ -31,31 +31,12 @@ public class ServiceController : Controller<IModel, IView>, IServiceController
         Debug.Log("ServiceController initialized successfully.");
     }
 
-    /// <summary>
-    /// Called once after the scene loads. 
-    /// Initializes this component by calling Init().
-    /// </summary>
-    /// <remarks>
-    /// Preconditions:
-    /// - Init() is implemented.
-    /// Postconditions:
-    /// - Init() is invoked.
-    /// </remarks>
+    /// <inheritdoc/>
     public void Awake() 
     {
         Init();
     }
 
-    /// <summary>
-    /// Called once after all Awake() calls.
-    /// This method does nothing, but overrides the default Start() defined in
-    /// Controller base class.
-    /// </summary
-    /// <remarks>
-    /// Preconditions:
-    /// - None
-    /// Postconditions:
-    /// - None
-    /// </remarks>
+    /// <inheritdoc cref="IServiceController" />
     public override void Start() {}
 }
