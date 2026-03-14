b1d979ad98e4b9605cfe2638939af522fa445b28
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-13 23:42:26 -0600

    [Chore] Added formal documentation to each header methods in IView
    Made an attempt towards formal documentation

--- VRGame/Assets/Scripts/Brain/IBrainView.cs
+++ VRGame/Assets/Scripts/Brain/IBrainView.cs
@@ -1,14 +1,37 @@
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
-// If you are making Model layer, inherit from IModel.
-// Same goes for other layers. (IController/IView)
-
 /// <summary>
-/// TODO: Change the docstring to match your implementation.
+///  Handles XR interaction events and passes them onto the controller
 /// </summary>
-public interface IBrainView : IModel
+public interface IBrainView : IView
 {
     /// <summary>
-    /// TODO: Change the docstring to match your implementation.
+    /// Initializes the Brain view component
     /// </summary>
     new void Init();
+
+    /// <summary>
+    /// Method sets up XR interaction events
+    /// </summary>
+    void setupXREvents();
+
+    /// <summary>
+    /// Calls controllerInstance.OnHoverEnter() on XR hover enter 
+    /// </summary>
+    /// <remarks>
+    /// Pre-condition:
+    ///     requires controllerInstance != null
+    /// Post-condition:
+    ///     ensures controllerInstance.OnHoverEnter() is called on XR hover enter
+    /// </remarks>
+    void OnXRHoverEnter(HoverEnterEventArgs args);
+
+    /// <summary>
+    /// Calls controllerInstance.OnHoverExit() on XR hover exit
+    /// </summary>
+    /// <remarks>
+    /// Pre-condition:
+    ///     requires controllerInstance != null
+    /// Post-condition:
+    ///     ensures controllerInstance.OnHoverExit() is called on XR hover exit
+    /// </remarks>
+    void OnXRHoverExit(HoverExitEventArgs args);
 }
