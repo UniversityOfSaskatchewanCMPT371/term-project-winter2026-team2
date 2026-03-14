d310cac0557a8031569aa335b54a528e8d3c8486
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-13 23:26:10 -0600

    [Chore] Added XR hover events for enter and exit in view component
    OnXRHoverEnter() calls on controller's OnHoverEnter() method
    OnXRHoverExit() calls on controller's OnHoverExit() method

--- VRGame/Assets/Scripts/Brain/BrainView.cs
+++ VRGame/Assets/Scripts/Brain/BrainView.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
-
 /// <summary>
 /// View component of BrainView.
 /// </summary>
-public class BrainView : 
-    View<IController>, // TODO reminder to switch the generic to the one you've implemented
-    IBrainView
+public class BrainView : View<IBrainController>, IBrainView
 {
-    // use 'this.controllerInstance' to access controller component
-
     /// <inheritdoc/>
     public override void Init()
     {
-        // this is used to resolve and validate the controller component
         this.CheckControllerRef();
     }
+
+    /// <inheritdoc>
+    private void OnXRHoverEnter(HoverEnterEventArgs args)
+    {
+        controllerInstance.OnHoverEnter();
+    }
+
+    /// <inheritdoc>
+    private void OnXRHoverExit(HoverExitEventArgs args)
+    {
+        controllerInstance.OnHoverExit();
+    }
+
 }
 
