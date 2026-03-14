33ce9e1157209a9932aa0894d39fcec9c8401076
charlesgamboa <charlesgamboa15@gmail.com>
2026-03-14 14:15:33 -0600

    [Chore] Edit Mode Tests
    Started on Edit Mode Tests

---
+++ VRGame/Assets/Tests/Service/EditMode/ServiceControllerTests.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ServiceControllerTest
+{
+    GameObject go;
+    ServiceController controller;
+
+    /// <summary>
+    /// Called once before every test functions. 
+    /// Handles setting up the test environment.
+    /// </summary>
+    [SetUp]
+    public void SetUp()
+    {
+        go = new GameObject();
+        controller = go.AddComponent<ServiceController>();
+    }
+
+    /// <summary>
+    /// Called once after every test functions.
+    /// Handles cleanning up the test environment.
+    /// </summary>
+    [TearDown]
+    public void TearDown()
+    {
+        Object.DestroyImmediate(go);
+    }
+
+    [Test]
+    public void Instantiation()
+    {
+        controller.Init();
+
+        // no errors should occur since it doesn't interact with
+        // any other layers
+    }
+
+    [Test]
+    public void Singleton()
+    {
+        controller.Init();
+
+        GameObject go2 = new GameObject();
+        go2.AddComponent<ServiceController>().Init();
+
+        // expected a warning since this is a duplicate of this singleton
+        // game object of the duplicate its attached to is expected to be destroyed
+    }
+}
