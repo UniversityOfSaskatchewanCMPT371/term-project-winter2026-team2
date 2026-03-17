b6434d9f148876ecb34c8013875678ba716e7f4a
Purity Ochang <purityochang07@gmail.com>
2026-03-17 17:57:27 -0600

    Added GameState MVC with interfaces using mvc script templates

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/GameStateModel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// Model component of GameStateModel.
+/// </summary>
+public class GameStateModel : Model, IGameStateModel
+{
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+
+    }
+}
---
+++ VRGame/Assets/Scripts/BlockBuilderGame/GameStateView.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// View component of GameStateView.
+/// </summary>
+public class GameStateView : 
+    View<IController>, // TODO reminder to switch the generic to the one you've implemented
+    IGameStateView
+{
+    // use 'this.controllerInstance' to access controller component
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+        // this is used to resolve and validate the controller component
+        this.CheckControllerRef();
+    }
+}
+
