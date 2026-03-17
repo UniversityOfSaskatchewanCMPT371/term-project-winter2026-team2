d5bdf217ec5adade97590c6ccd5ec648af066b5a
Purity Ochang <purityochang07@gmail.com>
2026-03-17 16:38:39 -0600

    Added block colour enum

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/Block/BlockColour.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// These are the available colours to apply to blocks
+/// </summary>
+public enum BlockColour
+{
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    purple,
+    pink,
+    brown,
+    grey,
+    black,
+    white
+}
