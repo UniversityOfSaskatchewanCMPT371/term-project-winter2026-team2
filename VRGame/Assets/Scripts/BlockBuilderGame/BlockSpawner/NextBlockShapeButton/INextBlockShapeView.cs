fa1752616bef93c81354578c366942819df22b13
Purity Ochang <purityochang07@gmail.com>
2026-03-19 16:31:00 -0600

    [feat] Added MVC Script template files
    These are of all of the buttons I would be implementing. The reason there isn't a model is because all of these are part of blockSpawner and they share a model.

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/BlockSpawner/NextBlockShapeButton/INextBlockShapeView.cs
@@ -0,0 +1,14 @@
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+// If you are making Model layer, inherit from IModel.
+// Same goes for other layers. (IController/IView)
+
+/// <summary>
+/// TODO: Change the docstring to match your implementation.
+/// </summary>
+public interface INextBlockShapeView : IModel
+{
+    /// <summary>
+    /// TODO: Change the docstring to match your implementation.
+    /// </summary>
+    new void Init();
+}
