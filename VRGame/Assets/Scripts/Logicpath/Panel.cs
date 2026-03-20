8b6aae77e2e3df0450e5e9c1441d8365907eaca9
leif-motif <140660659+leif-motif@users.noreply.github.com>
2026-03-19 21:37:57 -0600

    [chore] panel docstrings written

--- VRGame/Assets/Scripts/Logicpath/Panel.cs
+++ VRGame/Assets/Scripts/Logicpath/Panel.cs
@@ -6,7 +6,7 @@ using UnityEngine.Assertions;
 using UnityEngine.XR.Interaction.Toolkit;
 
 /// <summary>
-/// Represents a single panel in the logic path minigame
+/// Represents a single panel's attribute in the logic path minigame
 /// </summary>
 public enum PanelAttribute
 {
@@ -17,27 +17,66 @@ public enum PanelAttribute
 }
 
 /// <summary>
-/// Represents a single panel in the logic path minigame
-/// Manages pipe connections
+/// Represents a single panel in the logic path minigame - is essentially the "view" of the game
 /// </summary>
 public class Panel : MonoBehaviour, IEquatable<Panel>
 {
+    /// <summary>
+    /// The entry direction of this Panel, if any
+    /// </summary>
     private Direction entryDirection;
+    /// <summary>
+    /// The exit direction of this Panel, if any
+    /// </summary>
     private Direction exitDirection;
+    /// <summary>
+    /// The upper neighbor of this Panel, if there is one
+    /// </summary>
     private Panel topNeighbor;
+    /// <summary>
+    /// The right neighbor of this Panel, if there is one
+    /// </summary>
     private Panel rightNeighbor;
+    /// <summary>
+    /// The lower neighbor of this Panel, if there is one
+    /// </summary>
     private Panel downNeighbor;
+    /// <summary>
+    /// The left neighbor of this Panel, if there is one
+    /// </summary>
     private Panel leftNeighbor;
+    /// <summary>
+    /// Marks whether this Panel is a Block, start endpoint, end endpoint, or a regular Panel
+    /// </summary>
+
     [SerializeField]
     private PanelAttribute attribute;
+    /// <summary>
+    /// What colour this Panel has (if there is a path going through it or it is an endpoint)
+    /// </summary>
     [SerializeField]
     private PanelColour panelColour;
+    /// <summary>
+    /// The X-coordinate of this Panel
+    /// </summary>
     [SerializeField]
     private int gridX;
+    /// <summary>
+    /// The Y-coordinate of this Panel
+    /// </summary>
     [SerializeField]
     private int gridY;
+    /// <summary>
+    /// The texture manager for this Panel
+    /// </summary>
     private PanelTextureManager panelTextureManager;
+    /// <summary>
+    /// The Interactable used for hover events
+    /// </summary>
     private XRSimpleInteractable xRSimpleInteractable;
+    /// <summary>
+    /// The parenting game controller
+    /// </summary>
     private LogicGameController logicGameController;
 
     /// <summary>
@@ -152,7 +191,7 @@ public class Panel : MonoBehaviour, IEquatable<Panel>
     }
 
     /// <summary>
-    /// Accessor for panel attribute (normal, start, exit)
+    /// Accessor for panel attribute (normal, start, exit, block)
     /// </summary>
     public PanelAttribute Attribute
     {
@@ -240,6 +279,7 @@ public class Panel : MonoBehaviour, IEquatable<Panel>
     /// </preconditions>
     /// <postconditions>
     ///     - Resets entry and exit directions to None
+    ///     - Texture is refreshed
     /// </postconditions>
     /// </remarks>
     public void ClearPanel()
@@ -250,10 +290,12 @@ public class Panel : MonoBehaviour, IEquatable<Panel>
     }
 
     /// <summary>
-    /// Initializes the panel with grid coordinates and world position
+    /// Unity Awake() method, initializes the panel with grid coordinates, texture manager, XR interactable, and parenting game controller
     /// </summary>
     /// <remarks>
     /// <preconditions>
+    ///     - There is a PanelTextureManager and XRSimpleInteractable attached to this GameObject
+    ///     - There is a parenting GameObject with a LogicGameController attached to it
     /// </preconditions>
     /// <postconditions>
     /// </postconditions>
@@ -284,27 +326,50 @@ public class Panel : MonoBehaviour, IEquatable<Panel>
         Assert.IsNotNull(logicGameController, "Could not find LogicGameController!");
     }
 
+    /// <summary>
+    /// Unity OnDestroy() method, tears down hover event listeners
+    /// </summary>
     public void OnDestroy()
     {
         xRSimpleInteractable.hoverEntered.RemoveListener(OnHoverEntered);
         xRSimpleInteractable.hoverExited.AddListener(OnHoverExited);
     }
 
+    /// <summary>
+    /// Event listener function for hover start events
+    /// </summary>
+    /// <param name="args">Arguments for this event</param>
     private void OnHoverEntered(HoverEnterEventArgs args)
     {
         logicGameController.HandleHover(gridX, gridY);
     }
 
+    /// <summary>
+    /// Event listener function for hover end events
+    /// </summary>
+    /// <param name="args">Arguments for this event</param>
     private void OnHoverExited(HoverExitEventArgs args)
     {
         logicGameController.HandleUnhover(gridX, gridY);
     }
 
+    /// <summary>
+    /// Creates a string representation of this Panel
+    /// </summary>
+    /// <returns>A string representation of this Panel</returns>
     public override string ToString()
     {
         return $"({gridX},{gridY}), {panelColour}, {Attribute}, Entry {entryDirection}, Exit {exitDirection}";
     }
 
+    /// <summary>
+    /// Checks if this Panel is functionally equivalent to another Panel
+    /// </summary>
+    /// <param name="other">Another Panel to compare with</param>
+    /// <returns>true if the Panels are equivalent, false otherwise</returns>
+    /// <preconditions>
+    ///     - other != null
+    /// </preconditions>
     public bool Equals(Panel other)
     {
         return this.gridX == other.gridX && this.gridY == other.GridY && this.Attribute == other.Attribute && this.entryDirection == other.entryDirection && this.exitDirection == other.exitDirection && this.panelColour == other.panelColour;
