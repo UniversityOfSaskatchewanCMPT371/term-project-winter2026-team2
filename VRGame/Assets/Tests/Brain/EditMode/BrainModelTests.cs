3473de5f2b1cfe2eb451480a699ee155263fc1d9
Derek Wassill <dwassill@sasktel.net>
2026-03-21 18:34:40 -0600

    [Feat] Add more property tests Added more tests for properties of the brain model

--- VRGame/Assets/Tests/Brain/EditMode/BrainModelTests.cs
+++ VRGame/Assets/Tests/Brain/EditMode/BrainModelTests.cs
@@ -4,6 +4,7 @@ using FsCheck;
 using FsCheck.Fluent;
 using NUnit.Framework;
 using UnityEngine;
+
 public class BrainModelTests
 {
     /// <summary>
@@ -62,13 +63,92 @@ public class BrainModelTests
     /// Property based test to check pause() sets the animation speed to 0f
     /// </summary>
     [Test]
-    public void PausePropertyTest()
+    public void PauseAlwaysZeroTest()
     {
         Prop.ForAll<float>(speed =>
         {
             animator.speed = speed;
             brainModel.pause();
             return animator.speed == 0f;
+        }).QuickCheckThrowOnFailure();
+    }
+
+    /// <summary>
+    /// Property based test to check resume() sets the animation speed to 1.0f
+    /// </summary>
+    [Test]
+    public void ResumeAlwaysOneTest()
+    {
+        Prop.ForAll<float>(speed =>
+        {
+            animator.speed = speed;
+            brainModel.resume();
+            return animator.speed == 1.0f;
+        }).QuickCheckThrowOnFailure();
+    }
+
+
+    /// <summary>
+    /// Property based test to check pause() and resume() are idempotent 
+    /// (i.e. calling pause() multiple times keeps speed at 0f, calling resume() multiple times keeps speed at 1.0f)
+    /// </summary>
+    [Test]
+    public void PauseResumeIdempotentTest()
+    {
+        Prop.ForAll<float>(speed =>
+        {
+            animator.speed = speed;
+            brainModel.pause();
+            brainModel.pause();
+            return animator.speed == 0.0f;
+        }).QuickCheckThrowOnFailure();
+
+        Prop.ForAll<float>(speed =>
+        {
+            animator.speed = speed;
+            brainModel.resume();
+            brainModel.resume();
+            return animator.speed == 1.0f;
+        }).QuickCheckThrowOnFailure();
+    }
+
+    /// <summary>
+    /// Property based test to check that calling pause() is the same as calling resume() followed by pause()
+    /// 
+    /// </summary>
+    [Test]
+    public void PauseEqualsResumePauseTest()
+    {
+        Prop.ForAll<float>(speed =>
+        {
+            animator.speed = speed;
+            brainModel.resume();
+            brainModel.pause();
+            float speed1 = animator.speed;
+
+            animator.speed = speed;
+            brainModel.pause();
+            float speed2 = animator.speed;
+
+            return speed2 == speed1;
+        }).QuickCheckThrowOnFailure();
+    }
+
+    [Test]
+    public void ResumeEqualsPauseResumeTest()
+    {
+        Prop.ForAll<float>(speed =>
+        {
+            animator.speed = speed;
+            brainModel.pause();
+            brainModel.resume();
+            float speed1 = animator.speed;
+
+            animator.speed = speed;
+            brainModel.resume();
+            float speed2 = animator.speed;
+
+            return speed2 == speed1;
         }).QuickCheckThrowOnFailure();
     }
 }
