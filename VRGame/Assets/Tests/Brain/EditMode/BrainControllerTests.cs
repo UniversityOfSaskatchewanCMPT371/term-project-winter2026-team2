33c6a0d727c3a037fcaff35f15e57b737511c1d4
Derek Wassill <dwassill@sasktel.net>
2026-03-22 11:24:50 -0600

    [Feat] Add Brain Controller Tests Added tests to the

---
+++ VRGame/Assets/Tests/Brain/EditMode/BrainControllerTests.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NSubstitute;
+using System;
+using FsCheck;
+using FsCheck.Fluent;
+
+public class BrainControllerTests
+{
+    /// <summary>
+    /// The game object the component being test will be
+    /// attached to.
+    /// </summary>
+    GameObject go;
+
+    /// <summary>
+    /// The component that is being tested.
+    /// </summary>
+    BrainController controller;
+
+    IBrainModel mockModel;
+    IBrainView mockView;
+
+    /// <summary>
+    /// Called before each tests. Handles the setup for
+    /// game object and component being tested
+    /// </summary>
+    [SetUp]
+    public void Setup()
+    {
+        go = new GameObject();
+        BrainController controller = go.AddComponent<BrainController>();
+        mockModel = Substitute.For<IBrainModel>();
+        mockView = Substitute.For<IBrainView>();
+
+        controller.ModelMock = mockModel;
+        controller.ViewMock = mockView;
+    }
+
+    /// <summary>
+    /// Called after each tests. Handles the clean up
+    /// of game object.
+    /// </summary>
+    [TearDown]
+    public void TearDown()
+    {
+        UnityEngine.Object.DestroyImmediate(go);
+    }
+
+    /// <summary>
+    /// Property based test to verify that OnHoverEnter() make hoverCount increment by 1.
+    /// Property checked: n=hoverCount; OnHoverEnter(); hoverCount == n + 1
+    /// </summary>
+    [Test]
+    public void OnHoverEnterIncrements()
+    {
+        Prop.ForAll<NonNegativeInt>(count =>
+        {
+            controller.hoverCount = count.Get;
+            controller.OnHoverEnter();
+            return controller.hoverCount == count.Get + 1;
+        }).QuickCheckThrowOnFailure();
+    }
+
+    /// <summary>
+    /// Property based test to verify that OnHoverExit() make hoverCount decrement by 1 unless hoverCount is already 0.
+    /// Property checked: n=hoverCount; OnHoverExit(); if n == 0 then hoverCount == 0 else hoverCount == n - 1
+    /// </summary>
+    [Test]
+    public void OnHoverExitDecrementsUnlessZero()
+    {
+        Prop.ForAll<PositiveInt>(count =>
+        {
+            controller.hoverCount = count.Get;
+            controller.OnHoverExit();
+
+            if (count.Get == 0)
+            {
+                return controller.hoverCount == 0;
+            }
+            return controller.hoverCount == count.Get - 1;
+        }).QuickCheckThrowOnFailure();
+    }
+
+
+}
