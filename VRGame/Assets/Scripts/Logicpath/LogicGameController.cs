57436b6bd6aa4816427d38f85e9ae6b36f142ad5
leif-motif <140660659+leif-motif@users.noreply.github.com>
2026-03-22 20:05:11 -0600

    [fix] controller now has its own interface thingy

--- VRGame/Assets/Scripts/Logicpath/LogicGameController.cs
+++ VRGame/Assets/Scripts/Logicpath/LogicGameController.cs
@@ -25,15 +25,7 @@ public class LogicGameController : Controller<ILogicGameModel, Panel>, ILogicGam
     /// </summary>
     private Stack<Panel> currentPath;
 
-    /// <summary>
-    /// Gets initial interaction state set up
-    /// </summary>
-    /// <remarks>
-    /// preconditions:
-    ///     - A LogicGameModel script is attached to this object
-    /// postconditions:
-    ///     - All variables are initialized
-    /// </remarks>
+    /// <inheritdoc/>
     public override void Init()
     {
         isDragging = false;
@@ -94,23 +86,7 @@ public class LogicGameController : Controller<ILogicGameModel, Panel>, ILogicGam
     }
 
 #pragma warning disable IDE0051
-    /// <summary>
-    /// Handles a hover event from a Panel, changing the game state where necessary
-    /// </summary>
-    /// <param name="x">The X coordinate of a Panel</param>
-    /// <param name="y">The Y coordinate of a Panel</param>
-    /// <remarks>
-    /// preconditions:
-    ///     - X and Y are non-negative
-    ///     - X and Y are than LogicGameModel.MAX_GRID_SIZE
-    ///     - X and Y point to a panel in data
-    /// postconditions:
-    ///     - If we're not dragging, then no post-conditions
-    ///     - If we are dragging:
-    ///     - Cancels the current drag and resets the path if we change our hover to an occupied Panel
-    ///     - Cancels the current drag and resets the path if we change our hover to a non-adjacent Panel
-    ///     - Continues the current drag if we change our hover to an adjacent, non-occupied Panel
-    /// </remarks>
+    /// <inheritdoc/>
     public void HandleHover(int x, int y)
     {
         targetedPanel = new CoordinateRef(x,y);
@@ -182,17 +158,7 @@ public class LogicGameController : Controller<ILogicGameModel, Panel>, ILogicGam
         }
     }
 
-    /// <summary>
-    /// Handles the end of hovering on a Panel
-    /// </summary>
-    /// <param name="x">The X coordinate of a Panel</param>
-    /// <param name="y">The Y coordinate of a Panel</param>
-    /// <remarks>
-    /// preconditions:
-    ///     - None
-    /// postconditions:
-    ///     - If we're not hovering on a new panel, clear the targetedPanel coordinates
-    /// </remarks>
+    /// <inheritdoc/>
     public void HandleUnhover(int x, int y)
     {
         if(targetedPanel != null && targetedPanel.X == x && targetedPanel.Y == y)
@@ -202,18 +168,8 @@ public class LogicGameController : Controller<ILogicGameModel, Panel>, ILogicGam
         }
     }
 
-    /// <summary>
-    /// Handles pressing the (right) trigger
-    /// </summary>
-    /// <param name="context">The CallbackContext for this action</param>
-    /// <remarks>
-    /// preconditions:
-    ///     - None
-    /// postconditions:
-    ///     - If we're hovering over a non-occupied Panel, begin a drag movement
-    ///     - Otherwise, do nothing
-    /// </remarks>
-    private void OnTriggerPress(InputAction.CallbackContext context)
+    /// <inheritdoc/>
+    public void OnTriggerPress(InputAction.CallbackContext context)
     {
         Debug.Log("Trigger pressed!");
         if(targetedPanel == null)
@@ -240,19 +196,8 @@ public class LogicGameController : Controller<ILogicGameModel, Panel>, ILogicGam
         }
     }
 
-    /// <summary>
-    /// Handles releasing the (right) trigger
-    /// </summary>
-    /// <param name="context">The CallbackContext for this action</param>
-    /// <remarks>
-    /// preconditions:
-    ///     - None
-    /// postconditions:
-    ///     - If we are not in a dragging state, do nothing
-    ///     - If our drag ends on an endpoint, complete the drag movement
-    ///     - If our drag doesn't end on an endpoint (or any Panel), cancel the drag movement and clear the path
-    /// </remarks>
-    private void OnTriggerRelease(InputAction.CallbackContext context)
+    /// <inheritdoc/>
+    public void OnTriggerRelease(InputAction.CallbackContext context)
     {
         Debug.Log("Trigger released!");
         if(isDragging && (targetedPanel == null || currentPath.Peek().Attribute != PanelAttribute.Exit))
@@ -267,32 +212,15 @@ public class LogicGameController : Controller<ILogicGameModel, Panel>, ILogicGam
         isDragging = false;
     }
 
-    /// <summary>
-    /// Handles pressing the designated reset button
-    /// </summary>
-    /// <param name="context">The CallbackContext for this action</param>
-    /// <remarks>
-    /// preconditions:
-    ///     - None
-    /// postconditions:
-    ///     - The game state is reset
-    /// </remarks>
-    private void OnResetPress(InputAction.CallbackContext context)
+    /// <inheritdoc/>
+    public void OnResetPress(InputAction.CallbackContext context)
     {
         Debug.Log("Resetting game state...");
         modelInstance.ClearGrid();
     }
 
-    /// <summary>
-    /// Clear the current path we're drawing
-    /// </summary>
-    /// <remarks>
-    /// preconditions:
-    ///     - None
-    /// postconditions:
-    ///     - The current path being drawn is cleared by resetting the panels in the path and the path stack
-    /// </remarks>
-    private void ClearPath()
+    /// <inheritdoc/>
+    public void ClearPath()
     {
         Debug.Log("Clearing path!");
         foreach(Panel panel in currentPath)
