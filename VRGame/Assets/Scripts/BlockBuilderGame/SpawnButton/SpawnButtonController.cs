3fd2a49280e8cd8925755562744a536a3ee02ad1
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 12:33:49 -0600

    [Chore] Added Init() and OnButttonPressed() implementations in SpawnButtonController
    Init() initializes BlockSpawnerController component via interface and then OnButtonPressed() calls on BlockSpawnerController's SpawnBlock()

--- VRGame/Assets/Scripts/BlockBuilderGame/SpawnButton/SpawnButtonController.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/SpawnButton/SpawnButtonController.cs
@@ -1,22 +1,60 @@
 using UnityEngine;
-
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+using UnityEngine.Assertions;
 
 /// <summary>
 /// Controller component of SpawnButtonController.
+/// Manages the logic for triggering block spawning via button press.
 /// </summary>
-public class SpawnButtonController : 
-    Controller<IModel, IView>, // TODO reminder to switch the generics to the ones you've implemented
-    ISpawnButtonController
+public class SpawnButtonController : Controller<ISpawnButtonModel, ISpawnButtonView>, ISpawnButtonController
 {
-    // use 'this.viewInstance' to access view component, and
-    // 'this.modelInstance' to access model component
+    /// <summary>
+    /// Reference to the BlockSpawner controller (set via inspector window)
+    /// </summary>
+    [SerializeField] private ControllerComponent blockSpawnerController;
+
+    /// <summary>
+    /// Reference to the BlockSpawner controller to invoke SpawnBlock() on button press
+    /// </summary>
+    private IBlockSpawnerController blockSpawner;
+
+    /// <inheritdoc/>
+    public void Awake()
+    {
+        Init();
+    }
+
+    /// <inheritdoc/>
+    public override void Start() {}
 
     /// <inheritdoc/>
     public override void Init()
     {
-        // these are used to resolve and validate model and view components
         this.CheckModelRef();
         this.CheckViewRef();
+
+        // Implement from IBlockSpawnerController
+        if (blockSpawnerController is IBlockSpawnerController spawner)
+        {
+            blockSpawner = spawner;
+            Assert.IsNotNull(spawner, "spawner must not be null on Init");
+        }
+        else if (blockSpawnerController != null)
+        {
+            Debug.LogWarning("BlockSpawner controller does not implement from IBlockSpawnerController");
+        }
+
+        // Access BlockSpawnerController via its interface
+        if (blockSpawner == null)
+        {
+            blockSpawner = gameObject.GetComponent<IBlockSpawnerController>();
+            Assert.IsNotNull(blockSpawner, "blockSpawner must not be null on Init");
+        }
+    }
+
+    /// <inheritdoc/>
+    public void OnButtonPressed()
+    {
+        Assert.IsNotNull(blockSpawner, "blockSpawner must not be null on ButtonPressed");
+        blockSpawner.SpawnBlock();
     }
 }
