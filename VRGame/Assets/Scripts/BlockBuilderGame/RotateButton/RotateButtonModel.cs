8bbf6875258bb95d4b753e455940503a53c941d9
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 15:39:00 -0600

    [Chore] Added new mvc scripts and interfaces for rotate button
    inherited from script templates, none implemented yet

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/RotateButton/RotateButtonModel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// Model component of RotateButtonModel.
+/// </summary>
+public class RotateButtonModel : Model, IRotateButtonModel
+{
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+
+    }
+}
