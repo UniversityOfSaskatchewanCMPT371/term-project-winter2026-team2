f94e3803eaaf5655f579ee4dd002328f3e03e1ae
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 23:58:27 -0600

    [Chore] Implemented OnButtonPressed() in ColourController
    includes gaining access to all colours in the array set in inspector (via renderer)

--- VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/ColourController.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/ColourController.cs
@@ -44,5 +44,25 @@ public class ColourController : Controller<IColourModel, IColourView>, IColourCo
         Assert.IsNotNull(blockSpawnerModel, "'blockSpawnerModel' inspector field must be assigned to the Block Spawner GameObject.");
     }
 
-    
+    /// <inheritdoc/>
+    public void OnButtonPressed()
+    {
+        // Get access to the block spawned
+        GameObject block = blockSpawnerModel.LastSpawnedBlock;
+        Assert.IsNotNull(block, "LastSpawnedBlock must not be null to change colour");
+
+        // Get colours assigned in Inspector
+        Material[] colours = modelInstance.Colours;
+        Assert.IsNotNull(colours, "Colours must not be null");
+        Assert.IsTrue(colours.Length > 0, "Colours[] must not be empty");
+
+        // Get access to the renderer component (materials live on Renderer component)
+        int index = modelInstance.CurrentIndex;
+        Renderer renderer = block.GetComponentInChildren<Renderer>();
+        Assert.IsNotNull(renderer, "No Renderer found on LastSpawnedBlock");
+
+        // Set colour material based on index
+        renderer.material = colours[index];
+        modelInstance.CurrentIndex = (index + 1) % colours.Length;
+    }
 }
