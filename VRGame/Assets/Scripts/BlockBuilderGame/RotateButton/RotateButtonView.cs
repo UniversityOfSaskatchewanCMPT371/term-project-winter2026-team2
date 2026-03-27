bb9288b3da99a59c675f35aec460a1410b589855
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 20:48:25 -0600

    [Chore] Implemented RotateButtonView
    similar to SpawnButtonView

--- VRGame/Assets/Scripts/BlockBuilderGame/RotateButton/RotateButtonView.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/RotateButton/RotateButtonView.cs
@@ -1,21 +1,51 @@
 using UnityEngine;
-
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+using UnityEngine.Assertions;
+using UnityEngine.XR.Interaction.Toolkit;
 
 /// <summary>
 /// View component of RotateButtonView.
 /// </summary>
-public class RotateButtonView : 
-    View<IController>, // TODO reminder to switch the generic to the one you've implemented
-    IRotateButtonView
+public class RotateButtonView : View<IRotateButtonController>, IRotateButtonView
 {
     // use 'this.controllerInstance' to access controller component
 
     /// <inheritdoc/>
     public override void Init()
     {
-        // this is used to resolve and validate the controller component
         this.CheckControllerRef();
-    }
-}
 
+        SetupXREvents();
+    }
+
+    /// <inheritdoc/>
+    public void SetupXREvents()
+    {
+        if (controllerInstance == null)
+        {
+            Debug.LogWarning("Controller instance cannot be null on XR events setup");
+        }
+        Assert.IsNotNull(controllerInstance, "Controller must not be null on XR events setup");
+
+        var components = GetComponentsInChildren<XRBaseInteractable>();
+        if (components.Length == 0)
+        {
+            Debug.LogWarning("Cannot get components, none exist");
+        }
+        foreach (var c in components)
+        {
+            Assert.IsNotNull(c, "Null component found in components");
+            if (c == null)
+            {
+                Debug.LogError("Null component detected");
+            }
+            c.selectEntered.AddListener(OnXRClick);
+            Assert.IsNotNull(c, "Failed to add XR events to a (null) component");
+        }
+    }
+
+    private void OnXRClick(SelectEnterEventArgs args)
+    {
+        controllerInstance.OnButtonPressed();
+    }
+
+}
