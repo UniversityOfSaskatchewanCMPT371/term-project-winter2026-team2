eb92906d498f351aa04182984ceb1f2f8259e889
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-27 15:14:06 -0600

    [Chore] Added MVC scripts from script templates

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/BlockPlacer/BlockPlacerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// Controller component of BlockPlacerController.
+/// </summary>
+public class BlockPlacerController : 
+    Controller<IModel, IView>, // TODO reminder to switch the generics to the ones you've implemented
+    IBlockPlacerController
+{
+    // use 'this.viewInstance' to access view component, and
+    // 'this.modelInstance' to access model component
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+        // these are used to resolve and validate model and view components
+        this.CheckModelRef();
+        this.CheckViewRef();
+    }
+}
