6d7bca97fdc7129c2623fdc355a60d4252aed723
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 23:25:38 -0600

    [Chore] Updated IColourModel

--- VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/IColourModel.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/IColourModel.cs
@@ -1,14 +1,62 @@
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
-// If you are making Model layer, inherit from IModel.
-// Same goes for other layers. (IController/IView)
+using UnityEngine;
 
 /// <summary>
-/// TODO: Change the docstring to match your implementation.
+/// Interface for the ColourButton Model
 /// </summary>
 public interface IColourModel : IModel
 {
     /// <summary>
-    /// TODO: Change the docstring to match your implementation.
+    /// Array of (colour) materials to cycle through
     /// </summary>
+    Material[] Colours
+    {
+        /// <remarks>
+        /// pre-condition:
+        ///     - requires none
+        /// post-condition:
+        ///     - ensures return of colours[] of type Material
+        /// </remarks>
+        get;
+
+        /// <remarks>
+        /// pre-condition:  
+        ///     - requires (value != null) && (value.Length > 0)
+        /// post-condition:
+        ///     - ensures materials = value
+        /// </remarks>
+        set;
+    }
+
+    /// <summary>
+    /// Current index in the materials cycle.
+    /// </summary>
+    int CurrentMaterialIndex
+    {
+        /// <remarks>
+        /// pre-condition:
+        ///     - requires none
+        /// post-condition:
+        ///     - ensures currentMaterialIndex is returned
+        /// </remarks>
+        get;
+
+        /// <remarks>
+        /// pre-condition:
+        ///     - requires value >= 0
+        /// post-condition:
+        ///     - ensures currentMaterialIndex = value
+        /// </remarks>
+        set;
+    }
+
+    /// <summary>
+    /// Initializes currentMaterialIndex to 0.
+    /// </summary>
+    /// <remarks>
+    /// pre-condition:
+    ///     - requires none
+    /// post-condition:
+    ///     - ensures currentMaterialIndex == 0
+    /// </remarks>
     new void Init();
 }
