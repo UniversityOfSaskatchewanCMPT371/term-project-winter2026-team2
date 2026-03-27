2bbd96036ea1f7e322e16fe891e1eeb4d39e9693
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 22:17:09 -0600

    [Chore] Created new MVC scripts and interfaces for Color component
    inherited from ScriptTemplates

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/ColourModel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// Model component of ColourModel.
+/// </summary>
+public class ColourModel : Model, IColourModel
+{
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+
+    }
+}
