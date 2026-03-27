4054cb3ea4a15447b70ca058d65785127f791f36
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-26 22:30:45 -0600

    [Chore] Implemented Init and SetupXREvents in ColourView
    similar View implementation to SpawnView

--- VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/ColourView.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/ColourButton/ColourView.cs
@@ -10,12 +10,44 @@ public class ColourView :
     IColourView
 {
     // use 'this.controllerInstance' to access controller component
+public override void Init()
+    {
+        this.CheckControllerRef();
+
+        SetupXREvents();
+    }
 
     /// <inheritdoc/>
-    public override void Init()
+    public void SetupXREvents()
     {
-        // this is used to resolve and validate the controller component
-        this.CheckControllerRef();
+        if (controllerInstance == null)
+        {
+            Debug.LogWarning("Controller instance cannot be null on XR events setup");
+        }
+        Assert.IsNotNull(controllerInstance, "Controller must not be null on XR events setup");
+
+        // Assign listeners to all XR Base Interactable components 
+        var components = GetComponentsInChildren<XRBaseInteractable>();
+        if (components.Length == 0)
+        {
+            Debug.LogWarning("Cannot get components, none exist");
+        }
+        foreach (var c in components)
+        {
+            Assert.IsNotNull(c, "Null component found in components");
+            if (c == null)
+            {
+                Debug.LogError("Null component detected");
+            }
+            c.selectEntered.AddListener(OnXRClick);
+            Assert.IsNotNull(c, "Failed to add XR events to a (null) component");
+        }
     }
+
+    private void OnXRClick(SelectEnterEventArgs args)
+    {
+        controllerInstance.OnButtonPressed();
+    }
+    
 }
 
