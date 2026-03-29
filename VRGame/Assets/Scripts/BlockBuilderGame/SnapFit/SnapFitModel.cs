4cb39734368ff2bdeb0e2a018c3d2bbf3bca156f
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-28 19:12:44 -0600

    [Chore] Added SnapPoints in SnapPointModel.cs
    SnapPoint[] will contain the snap points of the block prefabs

--- VRGame/Assets/Scripts/BlockBuilderGame/SnapFit/SnapFitModel.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/SnapFit/SnapFitModel.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
-
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+using UnityEngine.Assertions;
 
 /// <summary>
 /// Model component of SnapFitModel.
 /// </summary>
 public class SnapFitModel : Model, ISnapFitModel
 {
+    private Transform[] snapPoints = new Transform[0];
+
+    /// <inheritdoc/>
+    public Transform[] SnapPoints
+    {
+        get
+        {
+            return snapPoints;
+        }
+        set
+        {
+            Assert.IsNotNull(value, "SnapPoint value must not be null");
+            snapPoints = value;
+        }
+    }
 
     /// <inheritdoc/>
     public override void Init()
