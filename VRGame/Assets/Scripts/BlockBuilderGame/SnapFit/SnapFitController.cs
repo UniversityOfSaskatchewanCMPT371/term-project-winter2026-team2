98a4b2a7d8d4e0f5f10c2aadd8eb610befe19e9a
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-28 22:00:12 -0600

    [Chore] Added Dettach, Snap, and FindSnapPoints in SnapFitController
    FindSnapPoints finds all the snap points attach to the block prefab,
    Detach() sets IsSnapped state to false
    Snap() sets state to true

--- VRGame/Assets/Scripts/BlockBuilderGame/SnapFit/SnapFitController.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/SnapFit/SnapFitController.cs
@@ -1,22 +1,62 @@
 using UnityEngine;
-
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections.Generic;
 
 /// <summary>
 /// Controller component of SnapFitController.
 /// </summary>
-public class SnapFitController : 
-    Controller<IModel, IView>, // TODO reminder to switch the generics to the ones you've implemented
-    ISnapFitController
+public class SnapFitController : Controller<ISnapFitModel, ISnapFitView>, ISnapFitController
 {
-    // use 'this.viewInstance' to access view component, and
-    // 'this.modelInstance' to access model component
+    /// <summary>
+    /// Initialize references and snap points on startup
+    /// </summary>
+    public void Awake()
+    {
+        Init();
+    }
 
     /// <inheritdoc/>
     public override void Init()
     {
-        // these are used to resolve and validate model and view components
+        // Check references
         this.CheckModelRef();
         this.CheckViewRef();
+
+        // Find snap points of the block prefab 
+        FindSnapPoints();
+    }
+
+    /// <inheritdoc/>
+    public void FindSnapPoints()
+    {
+        // List to hold snap points
+        var points = new List<Transform>();
+        foreach (Transform component in GetComponentsInChildren<Transform>())
+        {
+            // Find snap points of the block prefab by looking for components named "Top" or "Bottom"
+            if (component.name.StartsWith("Top") || component.name.StartsWith("Bottom"))
+            {
+                points.Add(component);
+            }
+        }
+
+        // Set snap points in model
+        modelInstance.SnapPoints = points.ToArray();
+        if (modelInstance.SnapPoints.Length == 0)
+        {
+            Debug.LogWarning("No snap points found for SnapFitController");
+        }
+    }
+
+    /// <inheritdoc/>
+    public void Detach()
+    {
+        modelInstance.IsSnapped = false;
+    }
+
+    /// <inheritdoc/>
+    public void Snap()
+    {
+        modelInstance.IsSnapped = true;
     }
 }
