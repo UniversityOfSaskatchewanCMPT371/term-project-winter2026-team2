20d8ad48f57a319973c92c5a462af9606d250efe
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-29 00:06:00 -0600

    [Chore] removed FindSnapPoints
    completely moved it to Init

--- VRGame/Assets/Scripts/BlockBuilderGame/SnapFit/SnapFitController.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/SnapFit/SnapFitController.cs
@@ -35,28 +35,6 @@ public class SnapFitController : Controller<ISnapFitModel, ISnapFitView>, ISnapF
     }
 
     /// <inheritdoc/>
-    public void FindSnapPoints()
-    {
-        // List to hold snap points
-        var points = new List<Transform>();
-        foreach (Transform component in GetComponentsInChildren<Transform>())
-        {
-            // Find snap points of the block prefab by looking for components named "Top" or "Bottom"
-            if (component.name.StartsWith("Top") || component.name.StartsWith("Bottom"))
-            {
-                points.Add(component);
-            }
-        }
-
-        // Set snap points in model
-        modelInstance.SnapPoints = points.ToArray();
-        if (modelInstance.SnapPoints.Length == 0)
-        {
-            Debug.LogWarning("No snap points found for SnapFitController");
-        }
-    }
-
-    /// <inheritdoc/>
     public void Detach()
     {
         // If not snapped or no snap joint, do nothing
