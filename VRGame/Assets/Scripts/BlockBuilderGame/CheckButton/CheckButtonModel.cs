aa8a260a7c71e62d934f22e405ad890e2e03622d
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-30 16:01:28 -0600

    [Chore] Added scanner animator data to CheckButtonModel
    as well as accessor method

--- VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/CheckButtonModel.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/CheckButtonModel.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+using UnityEngine.Assertions;
 /// <summary>
 /// Model component of CheckButton.
 /// No data needed — CheckArea owns the collider tracking.
@@ -6,4 +8,23 @@ public class CheckButtonModel : Model, ICheckButtonModel
 {
     /// <inheritdoc/>
     public override void Init() { }
+
+    /// <summary>
+    /// Reference to the CheckArea's scanner animator
+    /// </summary>
+    [SerializeField] private Animator scanner;
+
+    /// <inheritdoc/>
+    public Animator Scanner
+    {
+        get
+        {
+            return this.scanner;
+        }
+        set
+        {
+            Assert.IsNotNull(value, "Scanner reference cannot be null");
+            this.scanner = value;
+        }
+    }
 }
