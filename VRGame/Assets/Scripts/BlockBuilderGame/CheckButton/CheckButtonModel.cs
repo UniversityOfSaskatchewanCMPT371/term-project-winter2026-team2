cc5ad711f147d054ce519faae8ac539ce4dbafa8
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-29 19:33:54 -0600

    [Chore] Added CheckArea in CheckButtonModel
    a container that gets checked

--- VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/CheckButtonModel.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/CheckButtonModel.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
-
-// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+using UnityEngine.Assertions;
 
 /// <summary>
-/// Model component of CheckButtonModel.
+/// Model component of CheckButton.
+/// Sets up an area container for checking player's built blocks.
 /// </summary>
 public class CheckButtonModel : Model, ICheckButtonModel
 {
+    /// <summary>
+    /// The collider container that defines the area in which built blocks are checked
+    [SerializeField] private Collider checkArea;
+    
+    /// <inheritdoc/>
+    public Collider CheckArea
+    {
+        get
+        {
+            return checkArea;
+        }
+        set
+        {
+            Assert.IsNotNull(value, "CheckArea must not be null");
+            checkArea = value;
+        }
+    }
+
 
     /// <inheritdoc/>
     public override void Init()
     {
-
+        Assert.IsNotNull(checkArea, "CheckArea must be assigned in the Inspector");
     }
 }
--- VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/ICheckButtonModel.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/ICheckButtonModel.cs
@@ -1,10 +1,44 @@
-// <summary>
-/// TODO: Change the docstring to match your implementation.
+using UnityEngine;
+/// <summary>
+/// Interface for the CheckButton Model.
+/// Sets up an area container for checking player's built blocks.
 /// </summary>
 public interface ICheckButtonModel : IModel
 {
     /// <summary>
-    /// TODO: Change the docstring to match your implementation.
+    /// Initializes the model state
     /// </summary>
+    /// <remarks>
+    /// pre-condition:
+    ///     - requires none
+    /// post-condition:
+    ///     - ensures CheckArea != null
+    /// </remarks>
     new void Init();
+
+    /// <summary>
+    /// Accessor method for the collider container that defines the area in which built blocks are checked
+    /// </summary>
+    Collider CheckArea 
+    {
+        /// <summary>
+        /// Returns the collider container that defines the area in which built blocks are checked
+        /// </summary>
+        /// <remarks>
+        /// pre-condition:
+        ///     - requires none
+        /// post-condition:
+        ///     - ensures CheckArea is returned
+        /// </remarks>
+        get; 
+        /// <summary> 
+        /// Sets the collider container that defines the area in which built blocks are checked
+        /// </summary>
+        /// <remarks>
+        /// pre-condition:
+        ///     - requires value != null
+        /// post-condition:
+        ///     - ensures CheckArea == value
+        set; 
+        }
 }
