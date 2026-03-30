c8c585975d6f5700f9537a677aa2b5731dff2190
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-30 14:43:14 -0600

    [Chore] Created and initialized DeleteBlock MVC scripts and interfaces
    from script templates

---
+++ VRGame/Assets/Scripts/BlockBuilderGame/DeleteBlock/DeleteBlockModel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// TODO look at /VRGame/Assets/ScriptTemplate/Example.cs to see how to use this
+
+/// <summary>
+/// Model component of DeleteBlockModel.
+/// </summary>
+public class DeleteBlockModel : Model, IDeleteBlockModel
+{
+
+    /// <inheritdoc/>
+    public override void Init()
+    {
+
+    }
+}
---
+++ VRGame/Assets/Scripts/BlockBuilderGame/DeleteBlock/IDeleteBlockView.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// TODO: Change the docstring to match your implementation.
+/// </summary>
+public interface IDeleteBlockView : IView
+{
+    /// <summary>
+    /// TODO: Change the docstring to match your implementation.
+    /// </summary>
+    new void Init();
+}
