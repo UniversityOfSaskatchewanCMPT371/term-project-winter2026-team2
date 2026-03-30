f01ef58645da9f624034ddcd287e0a2016ef62dc
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-30 00:07:32 -0600

    [Chore] Updated documentation in ICheckButtonController

--- VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/ICheckButtonController.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/ICheckButtonController.cs
@@ -1,10 +1,38 @@
 /// <summary>
-/// TODO: Change the docstring to match your implementation.
+/// Interface for the CheckButton Controller.
 /// </summary>
 public interface ICheckButtonController : IController
 {
     /// <summary>
-    /// TODO: Change the docstring to match your implementation.
+    /// Calls Init to check for component references
     /// </summary>
+    /// <remarks>
+    /// pre-condition:
+    ///     - requires none
+    /// post-condition:
+    ///     - ensures Init() is called
+    /// </remarks>
+    new void Awake();
+
+    /// <summary>
+    /// Reference checks for model, view, TargetBlockController, and CheckAreaController
+    /// </summary>
+    /// <remarks>
+    /// pre-condition:
+    ///     - requires (modelInstance != null) && (viewInstance != null) && (targetBlockController != null)
+    /// post-condition:
+    ///     - ensures all references are checked
+    /// </remarks>
     new void Init();
+
+    /// <summary>
+    /// Counts the SnapFitControllers of the colliders in the CheckArea and calls CheckCompletion on the TargetBlockController with the found blocks
+    /// </summary>
+    /// <remarks>
+    /// pre-condition:
+    ///     - requires (modelInstance != null) && (viewInstance != null) && (targetBlockController != null)
+    /// post-condition:
+    ///     - ensures TargetBlockController.CheckCompletion is called (with the found blocks in the check area)
+    /// </remarks>
+    void OnButtonPressed();
 }
