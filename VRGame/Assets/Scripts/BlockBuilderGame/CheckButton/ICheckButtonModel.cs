77d4d60ba03c411dafb38ae626387164575fc373
antoinetterubia <antoinetterubia@yahoo.com>
2026-03-29 23:58:41 -0600

    [Chore] Updated CheckArea model interface
    removal of CheckArea

--- VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/ICheckButtonModel.cs
+++ VRGame/Assets/Scripts/BlockBuilderGame/CheckButton/ICheckButtonModel.cs
@@ -1,44 +1,11 @@
-using UnityEngine;
 /// <summary>
 /// Interface for the CheckButton Model.
-/// Sets up an area container for checking player's built blocks.
+/// Holds the reference to the CheckArea controller.
 /// </summary>
 public interface ICheckButtonModel : IModel
 {
     /// <summary>
     /// Initializes the model state
     /// </summary>
-    /// <remarks>
-    /// pre-condition:
-    ///     - requires none
-    /// post-condition:
-    ///     - ensures CheckArea != null
-    /// </remarks>
     new void Init();
-
-    /// <summary>
-    /// Accessor method for the collider container that defines the area in which built blocks are checked
-    /// </summary>
-    Collider CheckArea 
-    {
-        /// <summary>
-        /// Returns the collider container that defines the area in which built blocks are checked
-        /// </summary>
-        /// <remarks>
-        /// pre-condition:
-        ///     - requires none
-        /// post-condition:
-        ///     - ensures CheckArea is returned
-        /// </remarks>
-        get; 
-        /// <summary> 
-        /// Sets the collider container that defines the area in which built blocks are checked
-        /// </summary>
-        /// <remarks>
-        /// pre-condition:
-        ///     - requires value != null
-        /// post-condition:
-        ///     - ensures CheckArea == value
-        set; 
-        }
 }
