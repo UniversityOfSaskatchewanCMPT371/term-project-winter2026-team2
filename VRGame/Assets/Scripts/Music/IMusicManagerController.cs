d3ba538ee076bec672f5a70c782f979b1a41d92c
crh082 <crh082@usask.ca>
2026-04-02 15:01:00 -0600

    [Feat] Created the interface for the MusicManager
    Only contains Init() and Awake() methods

---
+++ VRGame/Assets/Scripts/Music/IMusicManagerController.cs
@@ -0,0 +1,31 @@
+/// <summary>
+/// Interface for the MusicManager component. Manages persistent music accross scenes
+/// </summary>
+public interface IMusicManagerController : IController
+{
+    /// <summary>
+    /// Initializes the music manager, sets up the AudioSource
+    /// </summary>
+    /// <remarks>
+    /// <pre-condition>
+    ///  - A MusicManager instance must not already exist in the scene
+    /// </pre-condition>
+    /// <post-condition>
+    ///  - A MusicManager instance is created and initialized in the scene
+    /// </post-condition>
+    /// </remarks>
+    new void Init();
+
+    /// <summary>
+    /// Called once after the scene loads
+    /// </summary>
+    /// <remarks>
+    /// <pre-condition>
+    ///  - None
+    /// </pre-condition>
+    /// <post-condition>
+    ///  - MusicManager is set to not be destroyed on load, allowing it to persist through scenes
+    /// </post-condition>
+    /// </remarks>
+    new void Awake();
+}
